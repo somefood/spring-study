0f9944c4b14c2d93500ca111c39ae66b019b4547
Seokju Hong <iamseokjuhong@gmail.com>
2021-08-18 14:11:55 +0900

    챕터6 수정내용 적용

--- sp5-chap06/src/main/java/config/AppCtxWithPrototype.java
+++ sp5-chap06/src/main/java/config/AppCtxWithPrototype.java
@@ -1,5 +1,19 @@
 package config;
 
-public class AppCtxWithPrototype {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
+import spring.Client;
+
+@Configuration
+public class AppCtxWithPrototype {
+	
+	@Bean
+	@Scope("prototype")
+	public Client client() {
+		Client client = new Client();
+		client.setHost("host");
+		return client;
+	}
 }
