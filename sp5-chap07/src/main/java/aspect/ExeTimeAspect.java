a99bb664f5048c92aee73a1ac0b557a458c129b4
Seokju Hong <iamseokjuhong@gmail.com>
2021-08-18 16:57:31 +0900

    챕터 8 진행중

--- sp5-chap07/src/main/java/aspect/ExeTimeAspect.java
+++ sp5-chap07/src/main/java/aspect/ExeTimeAspect.java
@@ -1,5 +1,33 @@
 package aspect;
 
-public class ExeTimeAspect {
+import java.security.Signature;
+import java.util.Arrays;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class ExeTimeAspect {
+	
+	@Pointcut("execution(public * chap07..*(..))")
+	private void publicTarget() {
+	}
+	
+	@Around("publicTarget()")
+	public Object measure(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.nanoTime();
+		try {
+			Object result = joinPoint.proceed();
+			return result;
+		} finally {
+			long finish = System.nanoTime();
+			Signature sig = joinPoint.getSignature();
+			System.out.printf("%s.%s(%s) 실행 시간 : %d ns\n",
+				joinPoint.getTarget().getClass().getSimpleName(),
+				sig.getName(), Arrays.toString(joinPoint.getArgs()),
+				(finish - start));
+		}
+	}
 }
---
+++ sp5-chap08/src/main/java/config/AppCtx.java
@@ -0,0 +1,30 @@
+package config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import spring.ChangePasswordService;
+import spring.MemberDao;
+import spring.MemberInfoPrinter;
+import spring.MemberListPrinter;
+import spring.MemberPrinter;
+import spring.MemberRegisterService;
+import spring.VersionPrinter;
+
+@Configuration
+public class AppCtx {
+    
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+    	DataSource ds = new DataSource();
+    	ds.setDriverClassName("com.mysql.jdbc.Driver");
+    	ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8");
+    	ds.setUsername("spring5");
+    	ds.setPassword("spring5");
+    	ds.setInitialSize(2);
+    	ds.setMaxActive(10);
+    	return ds;
+    }
+    
+}
