22f2e6fd464262393ce1201a1505fe67f9d4c505
Seokju Hong <iamseokjuhong@gmail.com>
2022-03-18 09:07:09 +0900

    Posts Repository test

---
+++ freelec-springboot2-webservice/src/test/java/com/jojoldu/book/freelecspringboot2webservice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.jojoldu.book.freelecspringboot2webservice.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void getPosts() {
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("somefood@naver.com")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
