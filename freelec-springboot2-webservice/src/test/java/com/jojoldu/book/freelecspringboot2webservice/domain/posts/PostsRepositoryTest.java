2abc219b72b7fae31684eded4d9e70e4f8337407
Seokju Hong <iamseokjuhong@gmail.com>
2022-03-19 21:06:43 +0900

    Add BaseTimeEntity

--- freelec-springboot2-webservice/src/test/java/com/jojoldu/book/freelecspringboot2webservice/domain/posts/PostsRepositoryTest.java
+++ freelec-springboot2-webservice/src/test/java/com/jojoldu/book/freelecspringboot2webservice/domain/posts/PostsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,5 +38,30 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         Assertions.assertThat(posts.getTitle()).isEqualTo(title);
         Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    void registerBaseTimeEntity() {
+
+        //given
+        LocalDateTime now = LocalDateTime.of(2022, 3, 19, 0, 0, 0 );
+        postsRepository.save(Posts.builder()
+                        .title("title")
+                        .content("content")
+                        .author("author")
+                        .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        Assertions.assertThat(posts.getCreatedDate()).isAfter(now);
+        Assertions.assertThat(posts.getModifiedDate()).isAfter(now);
+
+
     }
 }
