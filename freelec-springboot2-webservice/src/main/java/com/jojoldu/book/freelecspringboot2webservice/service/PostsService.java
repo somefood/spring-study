311c6c70492350e86965f408c27e0400df018b0d
Seokju Hong <iamseokjuhong@gmail.com>
2022-03-20 11:28:20 +0900

    basic CRUD

--- freelec-springboot2-webservice/src/main/java/com/jojoldu/book/freelecspringboot2webservice/service/PostsService.java
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/freelecspringboot2webservice/service/PostsService.java
@@ -2,12 +2,16 @@ package com.jojoldu.book.freelecspringboot2webservice.service;
 
 import com.jojoldu.book.freelecspringboot2webservice.domain.posts.Posts;
 import com.jojoldu.book.freelecspringboot2webservice.domain.posts.PostsRepository;
+import com.jojoldu.book.freelecspringboot2webservice.web.dto.PostsListResponseDto;
 import com.jojoldu.book.freelecspringboot2webservice.web.dto.PostsResponseDto;
 import com.jojoldu.book.freelecspringboot2webservice.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.freelecspringboot2webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,18 @@ public class PostsService {
                 new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
