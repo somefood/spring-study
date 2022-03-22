9d08bae3a39dfe32c95d8e48017a877de9ff401a
Seokju Hong <iamseokjuhong@gmail.com>
2022-03-22 16:42:53 +0900

    BaseEntityTime, RoleType 생성

---
+++ boardproject/src/main/java/com/somefood/boardproject/domain/RoleType.java
@@ -0,0 +1,16 @@
+package com.somefood.boardproject.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum RoleType {
+    ADMIN("ROLE_ADMIN", "관리자"),
+    MANAGER("ROLE_MANAGER", "매니저"),
+    USER("ROLE_USER", "유저");
+
+    private final String type;
+    private final String description;
+
+}
